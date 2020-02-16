4bc6dfc8f41b12c3f6be15ed64df0ce48ba5d890
Alexey Novichikhin <32311883+NovichikhinAlexey@users.noreply.github.com>
2020-02-16 18:29:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-kubectl get pods  -n $NAMESPACE --no-headers=true | awk '/$POD-/{print $1}' | xargs  kubectl delete -n $NAMESPACE pod
+kubectl get pods -n $NAMESPACE | grep $POD | cut -d " " -f1 | xargs kubectl delete pod -n $NAMESPACE
