170dda62d42638076f6e6b81bef3ca1c691675f6
Alexey Novichikhin <32311883+NovichikhinAlexey@users.noreply.github.com>
2020-02-16 18:12:02 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+# Extract the base64 encoded config data and write this to the KUBECONFIG
+echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+export KUBECONFIG=/tmp/config
+
+kubectl get pods  -n $NAMESPACE --no-headers=true | awk '/$POD-/{print $1}' | xargs  kubectl dele
+te -n $NAMESPACE pod
