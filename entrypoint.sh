2960760a7957f641b1f371fefaa7b8abb242d839
Sergey Korolev <sergey@korolevlive.com>
2020-05-06 09:10:37 +0300

    minor fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ set -e
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
 
-kubectl get pods -n $NAMESPACE | grep $POD | cut -d " " -f1 | xargs kubectl delete pod -n $NAMESPACE
+kubectl get pods -n $NAMESPACE | grep $POD | cut -d " " -f1 | head -1 | xargs kubectl delete pod -n $NAMESPACE
