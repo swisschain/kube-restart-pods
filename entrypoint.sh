eb2840d5a1ed7b32e915980d4ecf762c3ead3682
Sergey Korolev <sergey@korolevlive.com>
2020-06-16 15:56:54 +0300

    fix delete script

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,7 @@ set -e
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
 export KUBECONFIG=/tmp/config
-
-kubectl get pods -n $NAMESPACE | grep $POD | cut -d " " -f1 | head -1 | xargs kubectl delete pod -n $NAMESPACE
+RN=$(kubectl describe deployment $POD -n  $NAMESPACE|grep NewReplicaSet:|awk '{print $2}')
+PODS=$(for i in $(kubectl get pod -n $NAMESPACE| grep ${RN}|awk '{print $1}'); do echo $i; done)
+kubectl delete pod ${PODS} -n $NAMESPACE
+#kubectl get pods -n $NAMESPACE | grep $POD | cut -d " " -f1 | head -1 | xargs kubectl delete pod -n $NAMESPACE
