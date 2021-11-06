cef05a0211a20d23e42a93160390e693eec2c8c2
dvyu <dvyu@yandex.ru>
2021-11-06 20:06:14 +0500

    update ReplicaSet search

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
-RN=$(kubectl describe deployment $POD -n $NAMESPACE | grep ReplicaSet | grep -v "NewReplicaSetAvailable\|OldReplicaSets" | awk -F"ReplicaSet" '{print $2}' | sed 's#:##g' | sed 's#"##g' | awk '{print $1}')
+RN=$(kubectl describe deployment $POD -n $NAMESPACE | grep ReplicaSet | grep -v "NewReplicaSetAvailable\|OldReplicaSets\|ScalingReplicaSet" | awk -F"ReplicaSet" '{print $2}' | sed 's#:##g' | sed 's#"##g' | awk '{print $1}')
 echo ReplicaSet $RN
 PODS=$(for i in $(kubectl get pod -n $NAMESPACE| grep ${RN}|awk '{print $1}'); do echo $i; done)
 echo Pods $PODS
