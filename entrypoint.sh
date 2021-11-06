aa8f0b441bdae42c0d22062e7f49b0239821bbe7
dvyu <dvyu@yandex.ru>
2021-11-06 19:47:46 +0500

    update ReplicaSet search
    update ReplicaSet search for different kubernetes versions
    Vesion v1.13.4
    kubectl describe deployment service -n namespace|grep ReplicaSet
        Message:               ReplicaSet "service-86d977f947" has successfully progressed.
        Reason:                NewReplicaSetAvailable
    Version v1.16.15
    kubectl describe deployment service -n namespace|grep ReplicaSet
      Progressing    True    NewReplicaSetAvailable
    OldReplicaSets:  <none>
    NewReplicaSet:   service-75d659c479 (1/1 replicas created)

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
-RN=$(kubectl describe deployment $POD -n  $NAMESPACE|grep NewReplicaSet:|awk '{print $2}')
+RN=$(kubectl describe deployment $POD -n $NAMESPACE | grep ReplicaSet | grep -v "NewReplicaSetAvailable\|OldReplicaSets" | awk -F"ReplicaSet" '{print $2}' | sed 's#:##g' | sed 's#"##g' | awk '{print $1}')
 PODS=$(for i in $(kubectl get pod -n $NAMESPACE| grep ${RN}|awk '{print $1}'); do echo $i; done)
 kubectl delete pod ${PODS} -n $NAMESPACE
 #kubectl get pods -n $NAMESPACE | grep $POD | cut -d " " -f1 | head -1 | xargs kubectl delete pod -n $NAMESPACE
